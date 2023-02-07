963da1bac895315a9abb10bc271332a0b34a139e
Emile Choghi <emilechoghi@protonmail.com>
2023-02-06 16:11:28 -0800

    feat: new hooks + esm, cjs, and types build scripts

---
+++ src/useDocumentTitle/useDocumentTitle.test.ts
@@ -0,0 +1,22 @@
+import { renderHook } from '@testing-library/react';
+import { useDocumentTitle } from './useDocumentTitle';
+
+describe('useDocumentTitle', () => {
+  it('sets given value as document.title', () => {
+    renderHook(() => useDocumentTitle('test-title'));
+    expect(document.title).toBe('test-title');
+  });
+
+  it('does not change document.title if called with empty string', () => {
+    document.title = 'test-title';
+    renderHook(() => useDocumentTitle(''));
+    expect(document.title).toBe('test-title');
+    renderHook(() => useDocumentTitle('  \t\n'));
+    expect(document.title).toBe('test-title');
+  });
+
+  it('trims value before setting to document.title', () => {
+    renderHook(() => useDocumentTitle('  test-title\t\n   '));
+    expect(document.title).toBe('test-title');
+  });
+});
