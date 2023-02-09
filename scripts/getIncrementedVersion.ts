8b332d6ed6481731506b3ed96233f93425198998
Emile Choghi <emilechoghi@protonmail.com>
2023-02-09 08:53:22 -0800

    chore: automate release

---
+++ scripts/getIncrementedVersion.ts
@@ -0,0 +1,44 @@
+import chalk from 'chalk';
+
+const VERSION_INCREMENT: string[] = ['patch', 'minor', 'major'];
+
+export function getIncrementedVersion(version: string, options: { type: string }): string {
+  if (!VERSION_INCREMENT.includes(options.type)) {
+    chalk.red(
+      `Incorrect version type: ${chalk.cyan(
+        options.type,
+      )}, it should be one of these values: ${VERSION_INCREMENT.join(', ')}`,
+    );
+
+    process.exit(1);
+  }
+
+  const updateVersion = (raw: string): string => {
+    const splitted = raw.split('.');
+
+    if (options.type === 'patch') {
+      splitted[2] = (parseInt(splitted[2], 10) + 1).toString();
+    }
+
+    if (options.type === 'minor') {
+      splitted[1] = (parseInt(splitted[1], 10) + 1).toString();
+      splitted[2] = '0';
+    }
+
+    if (options.type === 'major') {
+      splitted[0] = (parseInt(splitted[0], 10) + 1).toString();
+      splitted[1] = '0';
+      splitted[2] = '0';
+    }
+
+    return splitted.join('.');
+  };
+
+  try {
+    return updateVersion(version);
+  } catch (e) {
+    chalk.red('Failed to parse package.json');
+    process.exit(1);
+    return '';
+  }
+}
---
+++ scripts/release.ts
@@ -0,0 +1,80 @@
+import path from 'path';
+import chalk from 'chalk';
+import simpleGit from 'simple-git';
+import githubRelease from 'new-github-release-url';
+import open from 'open';
+import { execa } from 'execa';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import { getIncrementedVersion } from './getIncrementedVersion';
+import { setPackageVersion } from './setPackageVersion';
+import packageJson from '../package.json';
+
+const git = simpleGit();
+const { argv }: { argv: any } = yargs(hideBin(process.argv))
+  .option('skip-build', {
+    type: 'boolean',
+    default: false,
+    description: 'Skip building step.',
+  })
+  .option('skip-publish', {
+    type: 'boolean',
+    default: false,
+    description: 'Skip publishing step.',
+  })
+  .example([['$0 minor --skip-build', 'Release but skip building packages.']]);
+
+(async () => {
+  const status = await git.status();
+
+  if (status.files.length !== 0) {
+    chalk.red('Working tree is not clean');
+    process.exit(1);
+  }
+
+  chalk.white('Releasing all packages');
+
+  let incrementedVersion = packageJson.version;
+
+  incrementedVersion = getIncrementedVersion(incrementedVersion, {
+    type: argv._[0] as string,
+  });
+  chalk.white(`New version: ${chalk.cyan(incrementedVersion)}`);
+
+  await setPackageVersion(incrementedVersion);
+
+  if (!argv.skipBuild) {
+    try {
+      await execa('yarn', ['build'], { stdio: 'inherit' });
+
+      chalk.green('Library was built successfully');
+    } catch (e) {
+      chalk.red('Build failed', e);
+      process.exit(1);
+    }
+  }
+
+  await git.add([path.join(__dirname, '../.')]);
+  await git.commit(`[release] Version: ${incrementedVersion}`);
+  await git.push();
+
+  if (!argv.skipPublish) {
+    try {
+      await execa('npm', ['publish'], { stdio: 'inherit' });
+
+      chalk.green('Library was successfully published to npm');
+    } catch (e) {
+      chalk.red('Publish failed', e);
+      process.exit(1);
+    }
+  }
+
+  open(
+    githubRelease({
+      user: 'Renna-Labs',
+      repo: 'hooks',
+      tag: incrementedVersion,
+      title: incrementedVersion,
+    }),
+  );
+})();
